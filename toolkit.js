6f04c9775da3b0c0263e476c289c3082cdc135e8
OXDBXKXO <l.chev@pm.me>
2021-02-08 16:51:54 +0100

    Added option for version checking Akamai on specific target (-v with no value will check every sites in config.json)

--- toolkit.js
+++ toolkit.js
@@ -9,28 +9,33 @@
 
 const { program, args } = require('commander');
 
-const { saveDeofbfuscatedFile, checkVersions, ternary2if } = require('./src/main')
-const { parse_sensor } = require('./src/parse-sensor');
+const { saveDeofbfuscatedFile, checkVersions, ternary2if, sensorParsing } = require('./src/main');
+const { getAkamaiVersion } = require('./src/akamai-script');
 
 
 async function args_parse() {
     
     program
     .option('-d, --deobfuscate <url>', "Fetches a fresh and readable copy of the Akamai script")
-    .option('-v, --version-check', "Checks current Akamai version on a large set of Akamai-protected websites")
-    .option('-t, -ternary', "Turns a ternary expression to a regular if-statement")
+    .option('-v, --version-check [url]', "Checks current Akamai version on a large set of Akamai-protected websites")
+    .option('-t, --ternary', "Turns a ternary expression to a regular if-statement")
     .option('-c, --custom-script <config_file>', "Prompts a Puppeteer-controlled Chromium instance for script behavior analysis")
-    .option('-p, --parse <sensor>', "Parses provided sensor_data")
+    .option('-p, --parse [sensor]', "Parses provided sensor_data")
 
     program.parse();
 
     const options = program.opts();
     
     if (options.deobfuscate) await saveDeofbfuscatedFile(options.deobfuscate);
-    if (options.versionCheck) await checkVersions();
-    if (options.Ternary) await ternary2if();
-    if (options.parse) parse_sensor(options.parse);
 
+    if (options.versionCheck) {
+        if (options.versionCheck === true) await checkVersions();
+        else { await getAkamaiVersion(options.versionCheck, log=true); process.exit(0); }
+    }
+
+    if (options.ternary) await ternary2if();
+
+    if (options.parse) sensorParsing();
 }
 
 args_parse();
